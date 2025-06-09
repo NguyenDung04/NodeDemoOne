f6783a5b4b6c8b1199d2b9572f298be795ff14fe
Heroic <nguyendung2004nb@gmail.com>
2025-06-09 22:45:33 +0700

    Cập nhật toàn bộ mã nguồn

--- src/index.js
+++ src/index.js
@@ -10,23 +10,21 @@ import { fileURLToPath } from 'url';
 
 const app = express()
 const port = 3000
-  
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Static files middleware
+app.use(express.static(path.join(__dirname, 'public')))
+
 // HTTP logger
 app.use(morgan('combined'))
 
 // Template engine setup
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 app.engine('hbs', engine({
   extname: ".hbs"
 }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
-
-// app.engine('handlebars', handlebars());
-// app.set('view engine', 'handlebars');
-// app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
   res.render('home');
