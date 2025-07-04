eb94153493d4439aaf7cbc60e9857f9987ed68ff
Heroic <nguyendung2004nb@gmail.com>
2025-07-04 22:32:49 +0700

    Cập Nhật Mã nguồn

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,9 @@
-import newsRouter from './news.js';
 import siteRouter from './site.js';
 import courseRouter from './course.js';
 import authRouter from './auth.js';
 
 function route(app) {
     app.use('/auth', authRouter);
-
-    app.use('/news', newsRouter);
 
     app.use('/course', courseRouter);
 
